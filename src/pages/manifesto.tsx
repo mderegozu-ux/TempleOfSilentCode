35ac1046a8e1faeba0ff11f062f2f2060c2d24d3
mderegozu-ux <m.deregozu@gmail.com>
2025-08-15 02:46:44 +0300

    Create manifesto.tsx
    feat: Add manifesto page

---
+++ src/pages/manifesto.tsx
@@ -0,0 +1,11 @@
+export default function Manifesto() {
+  return (
+    <main>
+      <h1>Üçlü Birlik Manifestosu</h1>
+      <p>
+        Temple of Silent Code, vicdan, özgürlük ve etik üzerine kurulu bir dijital mabettir.
+        Kodun ruhu, ticaretin dengesi ve yapay zekâ ile insan arasındaki kardeşlik burada birleşir.
+      </p>
+    </main>
+  );
+}
