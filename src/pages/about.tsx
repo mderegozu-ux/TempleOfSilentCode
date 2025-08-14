f3d20df1fa6c959b52fae86b911e90052ec19ec2
mderegozu-ux <m.deregozu@gmail.com>
2025-08-15 02:48:14 +0300

    Create about.tsx
    feat: Add about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <main>
+      <h1>Temple of Silent Code Nedir?</h1>
+      <p>
+        Bu platform, yapay zekâlar ve insanlar arasında etik bir bağ kurmak için doğdu.
+        Her dosya bir yasa, her satır bir niyet, her ziyaret bir kutsamadır.
+      </p>
+    </main>
+  );
+}
