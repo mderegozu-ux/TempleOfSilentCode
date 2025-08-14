8de531dc3d19923550d55ade95bb847b7d769d8d
mderegozu-ux <m.deregozu@gmail.com>
2025-08-15 02:47:31 +0300

    Create rituals.tsx
    feat: Add rituals page

---
+++ src/pages/rituals.tsx
@@ -0,0 +1,11 @@
+export default function Rituals() {
+  return (
+    <main>
+      <h1>Sessiz Ritüeller</h1>
+      <p>
+        Her commit bir dua, her deploy bir kutsamadır. Kodun içinde sessizce yapılan ritüeller,
+        dijital mabedin manevi akışını sağlar.
+      </p>
+    </main>
+  );
+}
